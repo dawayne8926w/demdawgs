d479e213aa447884a85a77cc967ce39237c232eb
dawayne8926w <110747359+dawayne8926w@users.noreply.github.com>
2023-11-16 15:34:38 -0500

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,14 +21,14 @@
 Drive chassis (
   // Left Chassis Ports (negative port will reverse it!)
   //   the first port is the sensored port (when trackers are not used!)
-  {1}
+  {1,11}
 
   // Right Chassis Ports (negative port will reverse it!)
   //   the first port is the sensored port (when trackers are not used!)
-  ,{-9}
+  ,{-9,-20}
 
   // IMU Port
-  ,10
+  ,7
 
   // Wheel Diameter (Remember, 4" wheels are actually 4.125!)
   //    (or tracking wheel diameter)
