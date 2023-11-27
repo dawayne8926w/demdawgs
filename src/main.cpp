8a73b2b03f95f7778bd3c3d7445dd2da4000e1a5
dawayne8926w <110747359+dawayne8926w@users.noreply.github.com>
2023-11-27 10:14:46 -0500

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,7 @@ Drive chassis (
 
   // External Gear Ratio (MUST BE DECIMAL)
   //    (or gear ratio of tracking wheel)
-  // eg. if your drive is 84:36 where the 36t is powered, your RATIO would be 2.333.
+  // eg. if your drive is 84:36 where the 36t is powered, your RANTIO would be 2.333.
   // eg. if your drive is 36:60 where the 60t is powered, your RATIO would be 0.6.
   ,1
 
